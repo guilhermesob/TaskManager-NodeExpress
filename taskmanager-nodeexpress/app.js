a8a6fcc79a65163269e180e8c74ee712c149e2bc
guilhermesob <101477908+guilhermesob@users.noreply.github.com>
2024-04-26 00:36:00 +0000

    revision pre  Live Server

--- taskmanager-nodeexpress/app.js
+++ taskmanager-nodeexpress/app.js
@@ -1,45 +1,44 @@
+// Importações de módulos necessários
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var tasksRouter = require('./routes/tasks');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+// Inicialização do aplicativo Express
 var app = express();
 
-// view engine setup
+// Configuração do diretório de visualizações e do motor de visualização
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug'); // Atualizado para 'pug', que é o novo nome do Jade
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// Configuração de middlewares
+app.use(logger('dev')); // Middleware para logging no ambiente de desenvolvimento
+app.use(express.json()); // Middleware para analisar o corpo das requisições JSON
+app.use(express.urlencoded({ extended: false })); // Middleware para analisar o corpo das requisições URL-encoded
+app.use(cookieParser()); // Middleware para analisar cookies
+app.use(express.static(path.join(__dirname, 'public'))); // Middleware para servir arquivos estáticos do diretório 'public'
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Definição das rotas
+app.use('/', require('./routes/index')); // Rota raiz
+app.use('/users', require('./routes/users')); // Rota para usuários
+app.use('/tasks', require('./routes/tasks')); // Rota para tarefas
 
-app.use('/tasks', tasksRouter);
 
-// catch 404 and forward to error handler
+
+// Tratamento de erros 404
 app.use(function(req, res, next) {
-  next(createError(404));
+ next(createError(404));
 });
 
-// error handler
+// Tratamento de erros gerais
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+ // Configuração de mensagens de erro e renderização de página de erro
+ res.locals.message = err.message;
+ res.locals.error = req.app.get('env') === 'development' ? err : {};
+ res.status(err.status || 500);
+ res.render('error');
 });
 
+// Exportação do aplicativo Express para uso em outros arquivos
 module.exports = app;
--- taskmanager-nodeexpress/routes/tasks.js
+++ taskmanager-nodeexpress/routes/tasks.js
@@ -7,6 +7,7 @@ var router = express.Router();
 var tasks = [
  { id: 1, name: 'Estudar Node.js', done: false },
  { id: 2, name: 'Desenvolver API', done: false },
+ { id: 3, name: 'Testar a API', done: false },
 ];
 
 // Rota para listar todas as tarefas
