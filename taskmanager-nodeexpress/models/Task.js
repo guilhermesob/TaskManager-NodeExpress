bf2e9dc227c2caee73247e08310810acf416bf0f
guilhermesob <101477908+guilhermesob@users.noreply.github.com>
2024-04-26 00:16:02 +0000

    add att models

---
+++ taskmanager-nodeexpress/models/Task.js
@@ -0,0 +1,21 @@
+//models/Task.js
+
+const mongoose = require('mongoose');
+
+const TaskSchema = new mongoose.Schema({
+ title: {
+    type: String,
+    required: true,
+    trim: true,
+ },
+ description: {
+    type: String,
+    trim: true,
+ },
+ completed: {
+    type: Boolean,
+    default: false,
+ },
+});
+
+module.exports = mongoose.model('Task', TaskSchema);
