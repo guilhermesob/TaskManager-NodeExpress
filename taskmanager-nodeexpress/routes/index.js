bd8bcfe68fde189ffb040f168b28928f23ccd30d
guilhermesob <101477908+guilhermesob@users.noreply.github.com>
2024-04-26 00:21:48 +0000

    feat : revisão andtes de ocnfigurar o server

--- taskmanager-nodeexpress/routes/index.js
+++ taskmanager-nodeexpress/routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const taskController = require('./controllers/taskController');
 
-/* GET home page. */
+// Rota para adicionar uma nova tarefa
+router.post('/tasks', taskController.createTask);
+
+// Rota para a página inicial
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+ res.render('index', { title: 'Express' });
 });
 
+// Exporta o router para ser usado em outros arquivos
 module.exports = router;
