4908997ca0c8636cd820f788c4f51895f28ec713
guilhermesob <101477908+guilhermesob@users.noreply.github.com>
2024-04-26 00:18:01 +0000

    revisão antes de adicionar routes

---
+++ taskmanager-nodeexpress/controllers/ taskController.js
@@ -0,0 +1,19 @@
+//taskController.js
+const Task = require('../models/Task');
+
+exports.createTask = async (req, res) => {
+  try {
+    const task = new Task(req.body);
+    await task.save();
+    res.status(201).json({
+      success: true,
+      data: task,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
